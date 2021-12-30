de836d7dcb05d7ab5bdd24adc71ed9401068989c
adarshpandya-simform <adarsh.p@simformsolutions.com>
2021-12-30 15:02:22 +0530

    TE5-T145 pollyfilly-reduce:added console.logs for understanding

--- js-advance-stuff/pollyfills-reduce.js
+++ js-advance-stuff/pollyfills-reduce.js
@@ -15,15 +15,27 @@ Array.prototype.customReduce = function (callback, initial = undefined) {
 };
 
 // reducing array with accumulator 'undefined'
+console.log(`-> reducing array with accumulator 'undefined'`);
 console.log(nums.customReduce((a, b) => a + b));
+
 // reducing array with accumulator '0'
+console.log(`-> reducing array with accumulator '0'`);
 console.log(nums.customReduce((a, b) => a + b, 0));
+
 // reducing array with accumulator '5'
+console.log(`-> reducing array with accumulator '5'`);
 console.log(nums.customReduce((a, b) => a + b, 5));
 
+console.log();
+
 // multiplying values with accumulator 'undefined'
+console.log(`~> multiplying values with accumulator 'undefined'`);
 console.log(nums.customReduce((a, b) => a * b));
+
 // multiplying values with accumulator '0'
+console.log(`~> multiplying values with accumulator '0'`);
 console.log(nums.customReduce((a, b) => a * b, 0));
+
 // multiplying values with accumulator '3'
+console.log(`~> multiplying values with accumulator '3'`);
 console.log(nums.customReduce((a, b) => a * b, 3));
