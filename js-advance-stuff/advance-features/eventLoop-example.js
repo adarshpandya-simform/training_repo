d873f733edd588bcec64462bf247de8398397349
adarshpandya-simform <adarsh.p@simformsolutions.com>
2021-12-31 15:42:54 +0530

    TE5-T145 event loop1

---
+++ js-advance-stuff/advance-features/eventLoop-example.js
@@ -0,0 +1,22 @@
+// !to run this file use following command in your terminal
+// `node ./js-advance-stuff/advance-features/filename.js`
+
+// event loop: the loop which iterates through
+// async queue and pull from there and push it
+// in call stack after the response of async function
+// is read
+
+// call stack
+// -------
+// |     |
+// -------
+// |     |
+// -------
+// |     |
+// -------
+// |     |
+// -------
+//  event queue which gets iterated by event loop
+//	-------------------------------------
+//  |		|		|		|		|		|		|		|		|		|
+//	-------------------------------------
