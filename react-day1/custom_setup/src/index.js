372f557d731af8938fadd2099f3b165f9d120c5b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-03 13:40:59 +0530

    TE5-T197 added react,react-dom

--- react-day1/custom_setup/src/index.js
+++ react-day1/custom_setup/src/index.js
@@ -1,3 +1,13 @@
-(() => {
-  console.log("hello world mitro");
-})();
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>hello world</h1>;
+};
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
--- react-day1/custom_setup/webpack.config.js
+++ react-day1/custom_setup/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
-  output: { path: path.join(__dirname, "build"), filename: "custom.bundle.js" },
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   mode: process.env.NODE_ENV || "development",
   resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
   devServer: {
