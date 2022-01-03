ca39c8dd3680e224abfd7609045163278fb00b38
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-03 17:01:25 +0530

    TE5-T197 added:fetch request on button click

--- react-day1/react-express/client/src/App.js
+++ react-day1/react-express/client/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const fetchData = async () => {
+    await fetch("http://localhost:2000/")
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.msg);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            fetchData();
+          }}
+        >
+          fetch data from backend
+        </button>
       </header>
     </div>
   );
