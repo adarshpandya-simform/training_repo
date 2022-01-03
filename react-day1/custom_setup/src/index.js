1755d0214d8ea0bb585e34cb684278f8001a30b2
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-03 13:58:50 +0530

    TE5-T197 added support for css, modular css and images

--- react-day1/custom_setup/src/index.js
+++ react-day1/custom_setup/src/index.js
@@ -1,16 +1,24 @@
 import React, { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import logo from "./assets/logo.svg";
+import styles from "./App.module.css";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <h1
-      onClick={() => {
-        setCounter((prevCount) => prevCount + 1);
-      }}
-    >
-      count : {counter}
-    </h1>
+    <>
+      <h1
+        className={styles.h1}
+        onClick={() => {
+          setCounter((prevCount) => prevCount + 1);
+        }}
+      >
+        count : {counter}
+      </h1>
+      <p>sample svg</p>
+      <img src={logo} alt="logo" />
+    </>
   );
 };
 
