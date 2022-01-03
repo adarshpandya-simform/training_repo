01a650148c2a098e8245bedf0722bcb782ae58fd
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-03 12:36:48 +0530

    TE5-T197 moved: index.js,index.html to src, added:webpack.config.js

---
+++ react-day1/custom_setup/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+  devServer: {
+    port: 3000,
+    hot: "only",
+    static: { directory: path.join(__dirname, "src/") },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
