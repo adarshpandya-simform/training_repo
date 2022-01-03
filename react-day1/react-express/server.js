54c22e9a4839022acc1dc3a072ac555434e9c6d4
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-03 17:11:58 +0530

    TE5-T197 added cors for different origin requests

--- react-day1/react-express/server.js
+++ react-day1/react-express/server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { config } from "dotenv";
 
 // configuring environment vars
 config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
