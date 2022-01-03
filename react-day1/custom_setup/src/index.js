e6a2eb19dd1f101ccbf759bb128c06a8a926f605
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-03 14:32:33 +0530

    TE5-T197 added: .editorconfig

--- react-day1/custom_setup/src/index.js
+++ react-day1/custom_setup/src/index.js
@@ -5,26 +5,27 @@ import logo from "./assets/logo.svg";
 import styles from "./App.module.css";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  return (
-    <>
-      <h1
-        className={styles.h1}
-        onClick={() => {
-          setCounter((prevCount) => prevCount + 1);
-        }}
-      >
-        count : {counter}
-      </h1>
-      <p>sample svg</p>
-      <img src={logo} alt="logo" />
-    </>
-  );
+	const [counter, setCounter] = useState(0);
+	console.log("ji");
+	return (
+		<>
+			<h1
+				className={styles.h1}
+				onClick={() => {
+					setCounter((prevCount) => prevCount + 1);
+				}}
+			>
+				count : {counter}
+			</h1>
+			<p>sample svg</p>
+			<img src={logo} alt="logo" />
+		</>
+	);
 };
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById("root")
 );
